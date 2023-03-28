8caf7ba8e7e6bbbef1e6783f0c72e2c50536f0e6
mrintroverrt <prasaanth2k@gmail.com>
2023-03-28 22:45:42 +0530

    Control Structors

---
+++ C_Control_Structors.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    int b = 20;
+    int c = 40;
+    if (a < b)
+    {
+        printf("Yes a is < than b");
+    }
+    else if (a == c)
+    {
+        printf("Yes a is Equal to c");
+    }
+    else
+    {
+        printf("No a is not less than b");
+    }
+    return 0;
+}
